43bbd927c24be62b7934ffe556f1005185d9dfde
chenxu <2853023689@qq.com>
2022-03-04 12:34:30 +0800

    增加 Polly 和 Trivial 的例子

---
+++ AspNetCore/Samples/PollyDemo/Controllers/RateLimitController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Polly.RateLimit;
+
+namespace PollyDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RateLimitController : ControllerBase
+    {
+        static int i = 0;
+
+        /// <summary>
+        /// 设置每10秒最多10次请求，最后一个参数如果不提供，则会把以差不多1秒1个的方式限制每个请求，如果提供，表示是否允许某一刻最多10个。
+        /// </summary>
+        static Polly.RateLimit.RateLimitPolicy policy = RateLimitPolicy.RateLimit(10, TimeSpan.FromSeconds(10), 10);
+
+        private readonly ILogger<RateLimitController> _logger;
+
+        public RateLimitController(ILogger<RateLimitController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                policy.Execute(() =>
+                    {
+                        _logger.LogInformation("xxx-" + (i++));
+                    });
+            }
+            catch (Exception ex)
+            {
+                var t = (ex).GetType();
+                throw;
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ AspNetCore/Samples/TrivialDemo/Controllers/CancellationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TrivialDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CancellationController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CancellationController> _logger;
+
+        public CancellationController(ILogger<CancellationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get(CancellationToken cancellationToken)
+        {
+            // 因为不是 await，所以这个请求会很快正常结束（cancellationToken 并不会被设置，所以下面的 break 是无效的）
+            Task.Run(() =>
+            {
+                // 注意，这个 Task.Run 是可以在 action 结束后继续执行的，
+                // 但是如果内部访问了很多 scoped 的服务，那当请求结束，这些服务会被 Dispose
+                // 如果不想 Dispose，那 Task 内部要手动创建一个新的 scope, see  https://stackoverflow.com/a/64892214/6294524
+                while (true)
+                {
+                    if(cancellationToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+
+                    System.Console.WriteLine(DateTime.Now.ToString("HH:mm:ss.fff"));
+                    Thread.Sleep(1000);
+                }
+            }, cancellationToken);
+
+            // 等待 5 s, 如果这期间结束这个请求（比如 关闭浏览器 tab 页），那么 cancellationToken 就会标志为 cancelled。
+            // 如果超过5s,那将永远无法结束上面的循环
+            Thread.Sleep(5000);
+            return Content("Ok");
+        }
+    }
+}
