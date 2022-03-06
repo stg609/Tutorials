3977edb3b865000b213ad48b442bb4fd8021e5c8
Charlie <stg609@outlook.com>
2022-03-06 19:36:20 +0800

    增加 CallerArgumentExpression

---
+++ CSharp/csharp10/CallerArgumentExpression/Program.cs
@@ -0,0 +1,14 @@
+﻿// net6 c#10
+
+// 用于输出入参的原始形式
+using System.Runtime.CompilerServices;
+
+Validate(1 == 2); // 会输出 1 == 2，而不是 false
+
+static void Validate(bool condition, [CallerArgumentExpression("condition")] string? message = null)
+{
+    if (!condition)
+    {
+        System.Console.WriteLine($"Argument failed validation: <{message}>");
+    }
+}
