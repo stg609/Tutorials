b8f303b3150a58ac684abc6525d7b692f6e05bfa
Charlie <stg609@outlook.com>
2022-03-14 20:21:21 +0800

    新增 多个 service invocation dapr 例子（草稿)

---
+++ Dapr/DaprSamples/DaprMultiContainer/MyFrontend/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFrontend.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DaprClient _daprClient;
+
+        public IndexModel(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+
+        public async Task OnGet()
+        {
+            // 这里使用了 service invocation building block
+            // 调用 MyBackend 服务的 weatherforecast 方法
+            var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+                HttpMethod.Get,
+                "MyBackend",
+                "weatherforecast");
+
+            ViewData["WeatherForecastData"] = forecasts;
+        }
+    }
+}
