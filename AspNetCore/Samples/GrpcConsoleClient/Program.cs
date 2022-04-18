230258cdb0d04cca4b55ecb9d252f6aad6d18084
Charlie <stg609@outlook.com>
2022-04-18 20:11:59 +0800

    完善注释

--- AspNetCore/Samples/GrpcConsoleClient/Program.cs
+++ AspNetCore/Samples/GrpcConsoleClient/Program.cs
@@ -17,6 +17,7 @@ namespace GrpcConsoleClient
             {
                 CodeBlock = "test",
 
+                // https://github.com/grpc/grpc-dotnet/issues/917#issuecomment-631765081
                 // 这里 ContextJson 最理想的是object类型，于是想到是否可以用 protocol 中的 Any
                 // 不能使用 Any 类型，因为 Any 其实需要 (Un)pack 一个消息，而这个消息任然需要定义的某个消息类型（因为需要实现 IMessage )
                 ContextJson = "xxx"
