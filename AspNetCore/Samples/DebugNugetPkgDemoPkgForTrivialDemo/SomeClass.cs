7b43113cd4088053478200f3f7dfcff9cdca7a55
Charlie <stg609@outlook.com>
2022-06-22 21:17:58 +0800

    提交 git commit，用于 snupkg 中的 Repository 节点信息

---
+++ AspNetCore/Samples/DebugNugetPkgDemoPkgForTrivialDemo/SomeClass.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace DebugNugetPkgDemoPkgForTrivialDemo
+{
+    public class SomeClass
+    {
+        public string SomeMethod()
+        {
+            string s = JsonConvert.SerializeObject(DateTime.Now);
+            return s;
+        }
+
+    }
+}
