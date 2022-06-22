1bf7df86aff1e081531c221ba003317001fb5ed1
Charlie <stg609@outlook.com>
2022-06-22 21:38:56 +0800

    部分支持调试自己的 nuget package

---
+++ AspNetCore/Samples/TrivialDemo/Controllers/DebugNugetPkgController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace TrivialDemo.Controllers
+{
+    /// <summary>
+    /// 演示如何调试 nuget pkg
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class DebugNugetPkgController : ControllerBase
+    {
+        private readonly ILogger<DebugNugetPkgController> _logger;
+
+        public DebugNugetPkgController(ILogger<DebugNugetPkgController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get(CancellationToken cancellationToken)
+        {
+            // F11，可以调试进去
+            
+            string str = JsonConvert.SerializeObject(DateTime.Now);
+
+
+            var sc = new DebugNugetPkgDemoPkgForTrivialDemo.SomeClass();
+            str = sc.SomeMethod();
+            return Content("Ok");
+        }
+    }
+}
---
+++ AspNetCore/Samples/TrivialDemo/Readme.txt
@@ -0,0 +1,7 @@
+﻿调试 nuget 包 （VS2022）
+1. 禁用 “启用仅我的代码”，
+2. 启用 源链接 （内部的子选项可以不勾）
+3. 在 调试-符号 页签中，启用 Microsoft 符号服务器（如果不想调试 .Net 的源码，可以不勾选），Nuget.org 符号服务器
+
+调试自己的nuget包
+1. 生成 symbol nuget 包： https://docs.microsoft.com/en-us/nuget/create-packages/symbol-packages-snupkg
