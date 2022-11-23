b23efd83a2846774c8a4024796afe7d2caa689cf
chenxu <2853023689@qq.com>
2022-11-23 10:43:42 +0800

    压缩测试

---
+++ CSharp/compression/CompressionDemo/DeCompressionBenchmarks.cs
@@ -0,0 +1,76 @@
+﻿using System.IO.Compression;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using K4os.Compression.LZ4;
+using K4os.Compression.LZ4.Streams;
+using ZstdNet;
+
+namespace CompressionDemo
+{
+    [MemoryDiagnoser]
+    public class DeCompressionBenchmarks
+    {
+        private byte[] _data;
+
+        [Params(1024 * 5, 1024 * 30, 1024 * 1204, 1024 * 1024 * 10)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _data = new byte[N];
+            new Random().NextBytes(_data);
+        }
+
+        [Benchmark]
+        public async Task Br()
+        {
+            await using var input = new MemoryStream(_data);
+            await using var output = new MemoryStream();
+            await using var stream = new BrotliStream(output, CompressionLevel.Fastest);
+
+            await input.CopyToAsync(stream);
+            await stream.FlushAsync();
+        }
+
+        [Benchmark]
+        public async Task Gzip()
+        {
+            await using var input = new MemoryStream(_data);
+            await using var output = new MemoryStream();
+            await using var stream = new GZipStream(output, CompressionLevel.Fastest);
+
+            await input.CopyToAsync(stream);
+            await stream.FlushAsync();
+        }
+
+        [Benchmark]
+        public void ZStd_Min()
+        {
+            using var compressor = new Compressor(new CompressionOptions(CompressionOptions.MinCompressionLevel));
+            var compressedData = compressor.Wrap(_data);
+        }
+
+        [Benchmark]
+        public void ZStd_Max()
+        {
+            using var compressor = new Compressor(new CompressionOptions(CompressionOptions.MaxCompressionLevel));
+            var compressedData = compressor.Wrap(_data);
+        }
+
+        [Benchmark]
+        public void Lz4_Encode()
+        {
+            var target = new byte[LZ4Codec.MaximumOutputSize(_data.Length)];
+            var encodedLength = LZ4Codec.Decode(
+                _data, 0, _data.Length,
+                target, 0, target.Length);
+        }
+
+        [Benchmark]
+        public void Lz4_Pickler()
+        {
+            LZ4Pickler.Unpickle(_data);
+        }
+    }
+}
---
+++ CSharp/compression/CompressionDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using CompressionDemo;
+using Perfolizer.Horology;
+
+Console.WriteLine("Hello, World!");
+
+//CompressionBenchmarks b = new CompressionBenchmarks();
+//b.Lz4_Encode_Compress();
+
+
+BenchmarkRunner.Run<CompressionBenchmarks>(
+    ManualConfig.Create(DefaultConfig.Instance)
+        .WithSummaryStyle(new BenchmarkDotNet.Reports.SummaryStyle(
+            cultureInfo: System.Globalization.CultureInfo.CurrentCulture,
+            printUnitsInHeader: true,
+            timeUnit: TimeUnit.Millisecond, sizeUnit: SizeUnit.KB))
+        );
